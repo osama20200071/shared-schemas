ffb7101b9596cb8bfea95c080e7158d8bbbfd467
Osama <osamakaram118@gmail.com>
2025-08-11 19:21:16 +0300

    Stop tracking dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/user.js';
-export * from './src/note.js';
-export * from './src/BecomeDriver/becomeDriver.js';
--- dist/src/note.js
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-export const noteSchema = z.object({
-    id: z.string(),
-    title: z.string(),
-    description: z.string(),
-});
